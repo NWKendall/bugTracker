c408bd456407e97b55c9b3ad0444ea37769bf2b2
nicbongo <nicbongo@protonmail.com>
2020-08-19 12:01:33 -0300

    login validation complete

--- backend/api/auth/auth.router.js
+++ backend/api/auth/auth.router.js
@@ -2,20 +2,11 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const UsersDB = require("./auth.model.js");
 const generateToken = require("./generateToken.js");
+const registerValidation = require("../middleware/registerValidation.js");
+const loginValidation = require("../middleware/loginValidation.js");
 
-router.get("/working", (req, res) => {
-  // const { id } = req.params;
-  console.log("XXX")
-  const email = "nic@test.com"
-  UsersDB.getUserByEmail(email).then((user) =>
-    res
-      .status(200)
-      .json(user)
-      .catch((err) => res.status(500).json(err))
-  );
-});
 
-router.post("/register", validateRegister, (req, res) => {
+router.post("/register", registerValidation, (req, res) => {
   let { first_name, last_name, password, email, role } = req.body;
   let hash = bcrypt.hashSync(password, 12);
   password = hash;
@@ -39,7 +30,7 @@ router.post("/register", validateRegister, (req, res) => {
     });
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", loginValidation, (req, res) => {
   let { email, password } = req.body;
 
   UsersDB.getUser({ email })
@@ -58,29 +49,3 @@ router.post("/login", (req, res) => {
 
 module.exports = router;
 
-async function validateRegister(req, res, next) {
-  const { email, password, role_id, first_name, last_name } = req.body;
-  const emailRegEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gim;
-  const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  const emailDuplicate = await UsersDB.getUserByEmail(email)
-  const errorMessages = []
-
-  for( const [key, value] of Object.entries(req.body )) {
-    if(!value) errorMessages.push(`${key} field is missing it's value. `)
-
-    if (key === "email") {
-      if (!value) errorMessages.push(`No email provided.`);
-      if (!emailRegEx.test(value)) errorMessages.push("Please provide a valid email address")
-      if (emailDuplicate) errorMessages.push("Email address is already being used. Please try another.")
-    }
-
-    if (key === "password") {
-      if (!value) errorMessages.push(`No password provided.`);
-      if (!passwordRegEx.test(value)) errorMessages.push("Password must contain: 8 characters minimum, one uppercase, one lowercase, 1 digit and 1 special character")
-    }
-  }
-
-  if (errorMessages.length) return res.status(400).json({errorMessages})
-
-  next();
-}
